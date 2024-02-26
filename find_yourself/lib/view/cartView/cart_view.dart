cb602e1d16b7b8bf3af8942f8e0dc682c44c8993
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2024-02-26 21:15:10 +0300

    Added Total Amount View Design

--- find_yourself/lib/view/cartView/cart_view.dart
+++ find_yourself/lib/view/cartView/cart_view.dart
@@ -1,3 +1,5 @@
+import 'package:find_yourself/core/constants/app_constants.dart';
+import 'package:find_yourself/view/cartView/widget/amount_view.dart';
 import 'package:flutter/material.dart';
 
 class CartView extends StatelessWidget {
@@ -6,9 +8,27 @@ const CartView({ Key? key }) : super(key: key);
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text("My Cart"),),
-      body: Text("My Cart"),
+      appBar: AppBar(title: const Text(AppConstants.myCartNavTitle),),
+      
+      body:  Column(children: [
+        const Expanded(flex: 1,child: Center(
+          child: Text("1 ${AppConstants.itemTitle}",
+          style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+
+          ))),
+        const Expanded(flex: 6,child: Center(child: Text("1 Item(s)"))),
+        Expanded(flex: 3,child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            ),
+            child: const AmountView()
+)
+        
+        )
+      ],)
       
       );
   }
 }
+
