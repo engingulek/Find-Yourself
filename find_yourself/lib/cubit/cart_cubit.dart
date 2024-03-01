699d3abcbc4785a1c8b81f8e3408b2ac4e1ffba3
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2024-03-01 19:55:51 +0300

    finished to add the product to cart

--- find_yourself/lib/cubit/cart_cubit.dart
+++ find_yourself/lib/cubit/cart_cubit.dart
@@ -23,19 +23,61 @@ class CartProductCubit extends Cubit<CartProductCubitEntity> {
       String productSize, 
       String productKey, 
       String productUrl) async{
+        Map<String,dynamic> data = {};
       
-   
-    /*_cartProducts.values.forEach((element) {
-      if(element.productId == productKey){
-        if (element.size == productSize) {
-          int newPiece = element.piece + 1;
+        
+        _cartProducts.values.forEach((element) async {
+          if (element.productId == productKey){
+           
+            for (int i = 0; i < element.sizes.length; i++){
+              if (element.sizes[i].size == productSize ){
+                var piece = element.sizes[i].piece;
 
-        }else{
-          print("size farklı eklenecek");
-        }
-      }else{
-        print("ilk defa eklenecek");
-      }
-    });*/
+                var newSize = Size(piece: piece + 1, size: productSize);
+                 var sizes = element.sizes;
+                 sizes.removeAt(i);
+                 sizes.add(newSize);
+                  data = {
+              "price": productPrice,
+              "productId": productKey,
+              "productName": productName,
+              "productUrl": productUrl,
+              "sizes":  sizes
+            };
+            var url = "https://marketdelivery-172e2-default-rtdb.firebaseio.com/cart/testUserid/${productKey}.json";
+              await Dio().patch(url,data:data);
+              }else{
+               var sizes = element.sizes;
+               sizes.add(Size(piece: 1, size: productSize));
+               data = {
+              "price": productPrice,
+              "productId": productKey,
+              "productName": productName,
+              "productUrl": productUrl,
+              "sizes":  sizes
+            };
+             var url = "https://marketdelivery-172e2-default-rtdb.firebaseio.com/cart/testUserid/${productKey}.json";
+              await Dio().patch(url,data:data);
+              }
+            }
+          }else{
+            data = {
+              "price": productPrice,
+              "productId": productKey,
+              "productName": productName,
+              "productUrl": productUrl,
+              "sizes": [
+                {
+                "piece": 1,
+                "size": productSize
+                }
+                ]
+            };
+             var url = "https://marketdelivery-172e2-default-rtdb.firebaseio.com/cart/testUserid.json";
+              await Dio().post(url,data:data);
+          }
+          
+        });
+        fetchCartProducs();
   }
 }
